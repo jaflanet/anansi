ae84f407192f9e2274980894fbbaaac42aab8d57
jonaa <56060812+jaflanet@users.noreply.github.com>
2024-09-27 14:41:19 +0700

    story-1

--- exercise/day-1/helloWorld/src/Main.kt
+++ exercise/day-1/helloWorld/src/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    println("Hello World!")
+    val nilai = arrayOf(90,89,79,69,59)
+
+    for(x in nilai) {
+        if (x >= 90) {
+            println("A")
+        } else if (x >= 80) {
+            println("B")
+        } else if (x >= 70) {
+            println("C")
+        } else if (x >= 60) {
+            println("D")
+        } else if (x < 60) {
+            println("F")
+        }
+    }
 }
