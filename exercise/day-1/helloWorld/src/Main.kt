61c9df700a04cef8c24feefee421d7aad03a294d
jonaa <56060812+jaflanet@users.noreply.github.com>
2024-09-27 14:49:30 +0700

    story-1

--- exercise/day-1/helloWorld/src/Main.kt
+++ exercise/day-1/helloWorld/src/Main.kt
@@ -1,17 +1,25 @@
 fun main() {
-    val nilai = arrayOf(90,89,79,69,59)
+    val nilai = arrayOf(90,89,79,69,59,-1,101)
 
     for(x in nilai) {
-        if (x >= 90) {
-            println("A")
-        } else if (x >= 80) {
-            println("B")
-        } else if (x >= 70) {
-            println("C")
-        } else if (x >= 60) {
-            println("D")
-        } else if (x < 60) {
-            println("F")
+        if (x in 90..100) {
+            print(x)
+            println(" = A")
+        } else if (x in 80..89) {
+            print(x)
+            println(" = B")
+        } else if (x in 70..79) {
+            print(x)
+            println(" = C")
+        } else if (x in 60..69) {
+            print(x)
+            println(" = D")
+        } else if (x in 0.. 60) {
+            print(x)
+            println(" = F")
+        }
+        else{
+            println("out of range")
         }
     }
 }
